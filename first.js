59fdb900757e3bbd05d9ad6bd180d859c3ac4562
Fazle Bari <fazlebarisn@gmail.com>
2021-06-13 21:18:38 +0600

    type of

--- first.js
+++ first.js
@@ -1,1 +1,11 @@
-console.log('Hello World');
+let num1 = 34
+let num2 = 3
+let result
+
+result = num1 + num2
+console.log( result )
+
+userName = "Fazle Bari"
+// How to know data type
+console.log( typeof result )
+console.log( typeof userName )
