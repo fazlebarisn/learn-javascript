5a45ec09a71a3deec3c51e1c808dca4267f8c042
Fazle Bari <fazlebarisn@gmail.com>
2021-12-20 13:06:23 +0600

    function inside an object

--- app.js
+++ app.js
@@ -1,30 +1,13 @@
-document.getElementById('hello').innerHTML = 'I am a man!';
-document.getElementById('hi').innerHTML = 'Cool!';
-document.getElementById('hi').style.color = 'red';
-
-document.getElementById('water').style.width = '500px';
-document.getElementById('water').style.height = '250px';
-
-// Function
-
-//let myName ='Sony';
-
-
-
-function newMe( myName ){
-    console.log( 'His name is ' + myName.name + ' He is ' + myName.age +' Years old' + ' And He is from ' + myName.vill);
-}
-
-const person1 = {
-    'name' : 'sony',
+const person = {
+    'firstName' : 'Fazle',
+    'lastName' : 'Bari',
+    'fullName' : function(){
+        return this.firstName + ' ' + this.lastName;
+    },
     'age' : 23,
     'vill' : 'Shihata'
 }
-const person2 = {
-    'name' : 'Rony',
-    'age' : 27,
-    'vill' : 'Dhaka'
-}
 
-newMe(person1);
-newMe(person2);
+console.log(person.fullName);
+
+document.getElementById("hi").innerHTML = person.fullName();
