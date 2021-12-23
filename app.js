16647383adf3e6caa766a4630adf8252f3904f1b
Fazle Bari <fazlebarisn@gmail.com>
2021-12-23 18:09:04 +0600

    Aro kaj ase

--- app.js
+++ app.js
@@ -1,26 +1,7 @@
 $(document).ready(function(){
 
-    $("#sname,#sclass,#country").focus(function(){
-        $(this).css("background-color" , "lime");
-    });
-
-    $("#sname,#sclass,#country").blur(function(){
-        $(this).css("background-color" , "");
-    });
-
-    $("#sname,#sclass").select(function(){
-        $(this).css("background-color" , "red");
-    });
-
-    $("#country").change(function(){
-        //$(this).css("background-color" , "pink");
-        var countryName = $(this).val();
-        console.log(countryName);
-        $("#extra").html(countryName)
-    });
-
-    $("#sform").submit(function(){
-        alert("Form Submited");
-    });
+  $(window).scroll(function(){
+      console.log("You are scrolling!");
+  })
 
 });
