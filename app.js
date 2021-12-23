88c0f412dc28b95e8d6d4ec08cb54fe56175f30b
Fazle Bari <fazlebarisn@gmail.com>
2021-12-23 15:00:20 +0600

    Done

--- app.js
+++ app.js
@@ -1,30 +1,12 @@
-document.getElementById('hello').innerHTML = 'I am a man!';
-document.getElementById('hi').innerHTML = 'Cool!';
-document.getElementById('hi').style.color = 'red';
-
-document.getElementById('water').style.width = '500px';
-document.getElementById('water').style.height = '250px';
-
-// Function
-
-//let myName ='Sony';
-
-
-
-function newMe( myName ){
-    console.log( 'His name is ' + myName.name + ' He is ' + myName.age +' Years old' + ' And He is from ' + myName.vill);
-}
-
-const person1 = {
-    'name' : 'sony',
-    'age' : 23,
-    'vill' : 'Shihata'
-}
-const person2 = {
-    'name' : 'Rony',
-    'age' : 27,
-    'vill' : 'Dhaka'
-}
-
-newMe(person1);
-newMe(person2);
+$(document).ready(function(){
+    // $('#list').css('border','2px solid red');
+    //$('#list li').css('border','1px solid red');
+    //$('.test').css('border','1px solid red');
+    //$('*').css('border','1px solid red');
+    // $("li:first").css("border" , "1px solid red");
+    // $("ul li:first-child").css("border" , "1px solid red");
+    // $("ul li:last").css("border" , "1px solid red");
+    // $("ul li:eq(2)").css("border" , "1px solid red");
+    // $("ul li:gt(1)").css("border" , "1px solid red");
+    $("ul li:lt(2)").css("border" , "1px solid red");
+});
