7a6f413e2e1d1185aa1bea7057cedf62d9c6a3bb
Fazle Bari <fazlebarisn@gmail.com>
2021-12-26 14:27:33 +0600

    done

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 $(document).ready(function(){
 
-  //var html = $('body').html();
-  //var html = $('#box').html();
+  /*
+  var html = $('body').html();
+  var html = $('#box').html();
   var html = $('#box p').html();
-  //console.log(html);
+  console.log(html);
 
   var text = $('#box p').text();
   console.log(text);
+
+  var attrs = $('#box').attr('class');
+  console.log(attrs);
+
+  */
+
+  // Get dara from 
+  $('#sform').submit(function(){
+
+    var name = $('#sname').val();
+    var sclass = $('#sclass').val();
+    var country = $('#country').val();
+    alert(name + sclass + country);
+
+  })
 
 });
