918cc2350b81ca4d757349e8861b193c7b5b7cbb
Fazle Bari <fazlebarisn@gmail.com>
2021-12-26 12:45:59 +0600

    ss

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
 
+  //var html = $('body').html();
+  //var html = $('#box').html();
+  var html = $('#box p').html();
+  //console.log(html);
 
+  var text = $('#box p').text();
+  console.log(text);
 
 });
