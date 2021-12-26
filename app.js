0015998d188e5a09baa99f83a7bdc1fc405a2251
Fazle Bari <fazlebarisn@gmail.com>
2021-12-26 14:51:58 +0600

    Done

--- app.js
+++ app.js
@@ -1,27 +1,16 @@
 $(document).ready(function(){
 
-  /*
-  var html = $('body').html();
-  var html = $('#box').html();
-  var html = $('#box p').html();
-  console.log(html);
+  $('#clickbutton').click(function(){
 
-  var text = $('#box p').text();
-  console.log(text);
+    $('#box h1').text("Hello there!");
+    //$('#box h1').text("Hello there!");
+    $('#box p').html("Dear <b>Fazle Bari</b>");
 
-  var attrs = $('#box').attr('class');
-  console.log(attrs);
-
-  */
-
-  // Get dara from 
-  $('#sform').submit(function(){
-
-    var name = $('#sname').val();
-    var sclass = $('#sclass').val();
-    var country = $('#country').val();
-    alert(name + sclass + country);
+    $('#box h1').attr("class" , "red" );
 
   })
+
+  $('#sname').val("Fazle Bari");
+  $('#sclass').val("Five");
 
 });
