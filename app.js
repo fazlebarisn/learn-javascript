1d14178433a79a237843d7e8888a970100cf2893
Fazle Bari <fazlebarisn@gmail.com>
2021-12-27 17:07:44 +0600

    done

--- app.js
+++ app.js
@@ -3,21 +3,8 @@ $(document).ready(function(){
   // Animation   
   $('#animateBtn').click(function(){
 
-    $('#box').animate({left : '250px'}, 3000);
-    $('#box').animate({fontSize : '25px'} , 2000);
+    $('#box').css('background' , 'pink').slideUp(2000).slideDown(2000);
 
   });
-
-  // Stop Animation   
-  $('#stopBtn').click(function(){
-
-    //$('#box').stop(); 
-
-    //$('#box').stop( true ); // If want to stop all animation
-
-    $('#box').stop( true , true ); // If want to stop all animation and jump at the end 
-
-  });
-
 
 });
