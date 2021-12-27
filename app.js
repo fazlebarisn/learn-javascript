550aeb23c0b746039356c428df8f49f19d2e2cc9
Fazle Bari <fazlebarisn@gmail.com>
2021-12-27 14:54:25 +0600

    done

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
 
-  console.log( $(window).scrollTop() );
+  //console.log( $(window).scrollTop() );
+  //console.log( $(window).scrollLeft() );
+
+  $(window).scroll(function(){
+
+    //console.log( $(window).scrollTop() );
+    //console.log( $(window).scrollLeft() );
+
+    $('#box').html('');
+    $('#box').append('Top: ' + $(window).scrollTop() + '<br> Left: ' + $(window).scrollLeft() );
+
+  })
+
+  // Set value on button click
+  $('#scrollTopBtn').click(function(){
+    $(window).scrollTop(200);
+  });
 
 });
