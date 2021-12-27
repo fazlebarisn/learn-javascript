b1e341d39aba0012d7a5dc12f8ca63fabf2ae85c
Fazle Bari <fazlebarisn@gmail.com>
2021-12-27 15:54:04 +0600

    done

--- app.js
+++ app.js
@@ -1,31 +1,48 @@
 $(document).ready(function(){
 
-  // Hide heading 
-  $('#hideBtn').click(function(){
+  // fadeIn heading 
+  $('#fadeInBtn').click(function(){
 
-    //$('#box h1').hide();
-    //$('#box h1').hide('fast');
-    //$('#box h1').hide(1000);
+    //$('#box h1').fadeIn();
+    //$('#box h1').fadeIn('slow');
+    //$('#box h1').fadeIn(2000);
 
-    $('#box h1').hide(1000 , function(){
-      console.log('Now it is hidden!');
+    $('#box h1').fadeIn(2000 , function(){
+      console.log('Fade In');
     });
 
   });
 
-  // Show heading
-  $('#showBtn').click(function(){
+  // FadeOut heading
+  $('#fadeOutBtn').click(function(){
 
-    //$('#box h1').show();
-    //$('#box h1').show('fast');
-    $('#box h1').show(1000);
+    //$('#box h1').fadeOut();
+    //$('#box h1').fadeOut('slow');
+    //$('#box h1').fadeOut(2000);
+
+    $('#box h1').fadeOut(2000 , function(){
+      console.log('Fade Out');
+    });
 
   });
 
   // Toggle heading
-  $('#toggleBtn').click(function(){
+  $('#fadeToggleBtn').click(function(){
 
-    $('#box h1').toggle();
+    $('#box h1').fadeToggle();
+
+  });
+
+  // Fade to
+  // $(selector).fadeTo( speed, opacity, callback ) fadeToBtn
+
+  $('#fadeToBtn').click(function(){
+
+    //$('#box').fadeTo( 2000, 0.2 );
+
+    $('#box').fadeTo(2000 , .2 ,function(){
+      console.log('Fade To');
+    });
 
   });
 
