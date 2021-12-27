05f6a3b28454ab9efd99a5290a289ca3c1bd1e7b
Fazle Bari <fazlebarisn@gmail.com>
2021-12-27 21:16:28 +0600

    test

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   // children selector   
   //$('#inner').children().css('background' , 'red' );
   //$('#outer').children().css('background' , 'red' ); 
-  //$('#inner').children('#child-c').css('background' , 'red' );
+  //$('#inner').children('#child-c').css('background' , 'red' ); 
 
   // find selector   
   $('#main-outer').find('.test').css('background' , 'red' ); 
