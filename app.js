2e881bda1fcf8a7c31264c7076b466393162ee43
Fazle Bari <fazlebarisn@gmail.com>
2021-12-27 11:55:13 +0600

    done

--- app.js
+++ app.js
@@ -2,21 +2,36 @@ $(document).ready(function(){
 
   $('#widthBtn').click(function(){
 
-    var divWidth = $('#box').width();
-    var divInnerWidth = $('#box').innerWidth();
-    var divOuterWidth = $('#box').outerWidth();
-    var divOuterWidthTrue = $('#box').outerWidth(true);
+    // check width
+    var Width = $('#box p').width();
+    var InnerWidth = $('#box p').innerWidth();
+    var OuterWidth = $('#box p').outerWidth();
+    var OuterWidthTrue = $('#box p').outerWidth(true);
 
-    console.log( "width : " + divWidth );
-    console.log( "innerWidth : " + divInnerWidth );
-    console.log( "outerwidth : " + divOuterWidth );
-    console.log( "outerwidth : " + divOuterWidthTrue );
+    console.log( "width : " + Width );
+    console.log( "innerWidth : " + InnerWidth );
+    console.log( "outerwidth : " + OuterWidth );
+    console.log( "outerwidth : " + OuterWidthTrue );
+
+    // Also can set width
+    $('#box').width('200px');
 
   })
 
   $('#heightBtn').click(function(){
 
-    $('#box p').unwrap();
+    var Height = $('#box').height();
+    var InnerHeight = $('#box').innerHeight();
+    var OuterHeight = $('#box').outerHeight();
+    var OuterHeightTrue = $('#box').outerHeight(true);
+
+    console.log( "Height : " + Height );
+    console.log( "InnerHeight : " + InnerHeight );
+    console.log( "OuterHeight : " + OuterHeight );
+    console.log( "OuterHeightTrue : " + OuterHeightTrue );
+
+    // Also can set height
+    $('#box').height('700px');
 
   })
 
