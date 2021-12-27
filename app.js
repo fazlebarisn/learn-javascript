6933a18fd818d5f39f14e1ca28c4c41e7131da72
Fazle Bari <fazlebarisn@gmail.com>
2021-12-27 12:56:26 +0600

    Scroll top

--- app.js
+++ app.js
@@ -1,34 +1,5 @@
 $(document).ready(function(){
 
-  $('#positionBtn').click(function(){
-
-    // check Position
-    //var position = $('#box').position();
-    var position = $('#box h1').position();
-
-    //console.log(position);
-
-    console.log(position.left); // will dispaly left value
-    console.log(position.top); // will display topo value
-
-  })
-
-  $('#offsetBtn').click(function(){
-
-    //var offset = $('#box').offset();
-    var offset = $('#box h1').offset();
-
-    console.log(offset);
-
-    console.log(offset.left); // will dispaly left value
-    console.log(offset.top); // will display topo value
-
-    // Also can set offset vallue
-    $('#box').offset({
-      top:0,
-      left:0
-    });
-
-  })
+  console.log( $(window).scrollTop() );
 
 });
