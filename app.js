d927e0540289094be9aa74df5a029f3b28beb8be
Fazle Bari <fazlebarisn@gmail.com>
2021-12-27 15:10:58 +0600

    done

--- app.js
+++ app.js
@@ -1,34 +1,17 @@
 $(document).ready(function(){
 
-  $('#positionBtn').click(function(){
+  $('#hasClassBtn').click(function(){
 
-    // check Position
-    //var position = $('#box').position();
-    var position = $('#box h1').position();
+    var hasClass = $('#box').hasClass('test');
 
-    //console.log(position);
+    //console.log(hasClass);
 
-    console.log(position.left); // will dispaly left value
-    console.log(position.top); // will display topo value
+    if(hasClass){
+      console.log('The class name is test');
+    }else{
+      console.log('There is no class');
+    }
 
-  })
-
-  $('#offsetBtn').click(function(){
-
-    //var offset = $('#box').offset();
-    var offset = $('#box h1').offset();
-
-    console.log(offset);
-
-    console.log(offset.left); // will dispaly left value
-    console.log(offset.top); // will display topo value
-
-    // Also can set offset vallue
-    $('#box').offset({
-      top:0,
-      left:0
-    });
-
-  })
+  });
 
 });
