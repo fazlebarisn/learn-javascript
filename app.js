6f6d55ebe4d55a91dfff00ff2c6ace38cb698681
Fazle Bari <fazlebarisn@gmail.com>
2021-12-27 20:58:25 +0600

    done

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 $(document).ready(function(){
 
   // Parent selector   
-  $('#child-c').parent().css('background' , 'red' );
+  //$('#child-c').parent().css('background' , 'red' );
+  //$('#inner').parent().css('background' , 'red' );
+
+  // Parents selector   
+  //$('#child-c').parents().css('background' , 'red' ); // will apply all parents
+  //$('#child-c').parents('#outer').css('background' , 'red' ); // will apply 'outer' parent
+  //$('#child-c').parentsUntil('#main-outer').css('background' , 'red' ); // will apply 'outer' to 'main-outer' parent
+
+  // offsetparent
+  //$('#child-c').offsetParent().css('background' , 'red' );
+
+  // closest
+  $('#child-c').closest('ul').css('background' , 'red' ); // will targate nerest 'ul'
 
 });
