8f0a027bb062b736badd474f7df4511fa8682bec
Fazle Bari <fazlebarisn@gmail.com>
2021-12-28 11:16:13 +0600

    done

--- app.js
+++ app.js
@@ -1,24 +1,11 @@
 $(document).ready(function(){
 
-  // has methord   
-  //$('p').has('span').css('background' , 'red' ); 
-  //$('p').has('b').css('background' , 'red' ); 
-  //$('p').has('b,span').css('background' , 'red' ); 
-  //$('p').has('.test').css('background' , 'red' ); 
+  $('#first ul li').each(function(){
 
-  // is methord
-  // if( $('p').parent().is('div') ){
-  //   console.log( $('p').parent().is('div') );
-  // }
-
-  $('p').click(function(){
-
-    if( $(this).parent().is('div') ){
-      $(this).css('background' , 'gold' );
-    }else{
-      $(this).css('background' , 'tan' );
-      
-    }
-  });
+    //console.log($(this).text());
+    var value = $(this).text();
+    $(this).text('Hello ' + value );
+    
+  })
 
 });
