4a19c83127240e57822a486d5acca3173aadd6b6
Fazle Bari <fazlebarisn@gmail.com>
2021-12-28 12:17:39 +0600

    done

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
 $(document).ready(function(){
 
-  $(document).mousemove(function(event){
-
-    $('#first').text('X:' + event.pageX + ' Y: ' + event.pageY);
-
-    let x = event.pageX;
-    let y = event.pageY;
-
-    $('.box').offset({top:x , left:y});
-
+  $('.box').on('click dblclick mouseover mouseout' , function(event){
+    $('h2').html('Event: ' + event.type);
   })
 
 });
