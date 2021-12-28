946f410fd704332855b0d33f84c6d4850ed98af7
Fazle Bari <fazlebarisn@gmail.com>
2021-12-28 10:18:44 +0600

    done

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 $(document).ready(function(){
 
-  // siblings selector   
-  //$('#child-c').siblings().css('background' , 'red' );
-  //$('#child-c').siblings('h2').css('background' , 'red' );
+  // first methord   
+  //$('p').first().css('background' , 'red' ); 
+  //$('#second p').first().css('background' , 'red' ); 
+  //$('ul li').first().css('background' , 'red' );
 
-  // next selector   
-  //$('#child-c').next().css('background' , 'red' ); 
-  //$('#child-c').nextAll().css('background' , 'red' ); 
-  //$('#child-c').nextUntil('.testy').css('background' , 'red' );
+  // last selector
+  //$('p').last().css('background' , 'red' ); 
+  //$('#first p').last().css('background' , 'red' ); 
+  //$('ul li').last().css('background' , 'red' );
 
-  // prev selector   
-  //$('#child-c').prev().css('background' , 'red' ); 
-  //$('#child-c').prevAll().css('background' , 'red' ); 
-  $('#child-c').prevUntil('test').css('background' , 'red' ); 
+  // eq selector
+  //$('ul li').eq(2).css('background' , 'red' );
+  //$('ul li').eq(-1).css('background' , 'red' ); // will select the last element
+
+  // filter
+  //$('p').filter('.test').css('background' , 'red' );
+  //$('p.test').css('background' , 'red' );
+
+  // not selector
+  //$('p').not('.test').css('background' , 'red' );
+
+  // slice
+  //$('ul li').slice(1).css('background' , 'red' ); // will tergate from index of 1
+  $('ul li').slice(1,3).css('background' , 'red' ); // will tergate from index of 1 to index of 3
 
 });
