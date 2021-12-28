dd23a7e80a32a2fba3dcb29f7b4ae60b071ce501
Fazle Bari <fazlebarisn@gmail.com>
2021-12-28 14:27:55 +0600

    start

--- app.js
+++ app.js
@@ -1,23 +1,5 @@
 $(document).ready(function(){
 
-  $('.box').on('click' , function(event){
-    alert('div is checked!');
-  });
-
-  $('h2').on('click' , function(event){
-    alert('h2 is checked!');
-  });
-
-  $('p').on('click' , function(event){
-
-    event.stopPropagation();
-    //alert("p is checked!");
-    alert("p is checked! " + event.isPropagationStopped() ); // to check 
-
-  });
-
-  $('button').on('click' , function(event){
-    alert('button is checked!');
-  })
+  
 
 });
