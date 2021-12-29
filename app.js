f44010a480105c83c9ca9c4154bd421a032e0e6d
Fazle Bari <fazlebarisn@gmail.com>
2021-12-29 18:12:07 +0600

    fff

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 $(document).ready(function(){
 
+  let id = 1;
   $('#add').click(function(){
 
-    let html = "<tr><td>Name</td><td><input type='text'><button id='remove'>Remove</button></td></tr>";
+    id++;
+
+    let html = "<tr id='tr"+id+"'><td>Name</td><td><input type='text'><button id='remove'>Remove</button></td></tr>";
     $('#tbl').append(html);
+
+    $('#remove').click(function(){
+
+      
+      let ttt = "tr" + id;
+      console.log(ttt);
+      $('#ttt').remove();
+  
+    });
     
   });
 
-  $('#remove').click(function(){
-
-    alert(11111);
-    $('#remove').remove();
-
-  });
 
 });
