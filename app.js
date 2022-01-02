3de402e39695998a168f08c7a50cef93942eef4f
Fazle Bari <fazlebarisn@gmail.com>
2022-01-02 10:08:55 +0600

    dd

--- app.js
+++ app.js
@@ -1,23 +1,61 @@
 $(document).ready(function(){
 
-  let id = 1;
-  $('#add').click(function(){
+ $("#box select").change(function(){
+  var selectedCountry = $(this).children("option:selected").val();
 
-    id++;
-
-    let html = "<tr id='tr"+id+"'><td>Name</td><td><input type='text'><button id='remove'>Remove</button></td></tr>";
-    $('#tbl').append(html);
-
-    $('#remove').click(function(){
-
-      
-      let ttt = "tr" + id;
-      console.log(ttt);
-      $('#ttt').remove();
-  
-    });
-    
+  console.log(selectedCountry);
   });
-
+  let x = 44444;
+alert(x.toString().length);
+  
 
 });
+
+
+// function add_four_digit_value(){
+//   ?>
+//   <script>
+//       jQuery(document).ready(function(){
+      
+        $(document.body).on('change','.wpt_product_table_wrapper table tr td .wpt_varition_section select',function(){
+          $('.table-inputbox').val("");
+        });
+      
+      
+      jQuery(".table-inputbox").after('<div class="demo"></div>');
+      
+      jQuery(".wpt_woo_add_cart_button").on("click" , function(event){
+      var value = jQuery(".table-inputbox").val();
+      if(value ==""){
+        alert("Enter Value");
+        jQuery(".wpt_woo_add_cart_button").removeClass("enabled ");
+        event.preventDefault();
+      }	
+    });
+      
+  
+        jQuery(".table-inputbox").keyup(function(event){
+  
+          var value = jQuery(this).val();
+          var countN = (value.match(/([0-9])/g) || []).length;
+  
+          if( countN !== 4){
+            jQuery(".demo").text("Enter four digit only");
+            jQuery(".wpt_woo_add_cart_button").click(function(event){
+              jQuery(this).removeClass("enabled ");
+              event.preventDefault();
+            });
+          }else if( countN == 4 ){
+            jQuery(".wpt_woo_add_cart_button").click(function(event){
+              jQuery(this).addClass("enabled ");
+            });
+            jQuery(".demo").hide();
+          }
+        });
+            
+//     });
+//   </script>
+//   <?php	
+//   }
+  
+//   add_action('wp_head','add_four_digit_value');
