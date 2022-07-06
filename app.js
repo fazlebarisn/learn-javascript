0b04a793bbf2d2dcd63f5cd188833de2183707cd
Fazle Bari <fazlebarisn@gmail.com>
2022-07-06 16:00:50 +0600

    ff

--- app.js
+++ app.js
@@ -33,10 +33,89 @@ cars.push("Farari");
 //     console.log(val);
 // }
 
-const points = [40,100,1,5,25,10,1.5];
-points.sort();
-points.sort(function(a,b){
-    return a-b;
-})
+// const points = [40,100,1,5,25,10,1.5];
+// // let sr = ['8th','1st','25th','2nd','26th','3rd'];
 
-document.getElementById('demo').innerHTML = points;
+// points.sort();
+// // sr.sort();
+
+// // var inputText = "'foofo21' 'bar432' 'foobar12345'";
+// var inputText = ['8th','1st','25th','2nd','26th','3rd'];
+// function processText(inputText) {
+//     var output = [];
+//     var json = inputText.split(' ');
+//     json.forEach(function (item) {
+//         output.push(item.replace(/\'/g, '').split(/(\d+)/).filter(Boolean));
+//     });
+//     return output;
+// }
+
+// console.log(JSON.stringify(processText(inputText)));
+
+// points.sort(function(a,b){
+//     return a-b;
+// })
+
+// document.getElementById('demo').innerHTML = sr;
+
+// program to extract value as an array from an array of objects
+
+// function extractValue(arr, prop) {
+
+//     // extract value from property
+//     let extractedValue = arr.map(item => item[prop]);
+
+//     return extractedValue;
+
+// }
+
+// const objArray = [{a: 1, b: 2}, {a: 4, b: 5}, {a: 8, b: 9}];
+
+// // passing an array of objects and property 'a' to extract
+// const result = extractValue(objArray, 'a');
+// console.log(result);
+
+ var array = ['8th','1st','25th','2nd','26th','3rd'];
+
+// arr.forEach(myFunction);
+
+// function myFunction(val){
+//     var numb = val.match(/\d/g);
+// }
+// console.log(arr);
+
+// function arrayRemove(arr, value) { 
+    
+//     return arr.filter(function(ele){ 
+//         //console.log(ele);
+//         var numb = ele.match(/\d/g);
+//         console.log(numb);
+//         //return numb; 
+//     });
+// }
+
+// var result = arrayRemove(array, 6);
+
+//console.log(result);
+// result = [1, 2, 3, 4, 5, 7, 8, 9, 0]
+
+//var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var filtered = array.filter(function(value, index, arr){ 
+    //console.log(value, index,arr );
+
+    //var numb = value.replace(/[^0-9]/g,'');
+    var numb = value.match(/\d/g);
+    parseInt(numb)
+    //console.log( numb);
+    return numb > 5;
+});
+
+console.log(filtered);
+
+
+
+// function arrayFromArgs() {
+//     return Array.prototype.slice.call(arguments);
+// }
+// var fruits = arrayFromArgs('Apple', 'Orange', 'Banana');
+// console.log(fruits);
